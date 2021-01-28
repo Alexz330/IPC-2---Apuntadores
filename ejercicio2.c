0c5ae471d6faf9d4b71e5d2e893ce90ad5c63860
Alexz330 <72354711+Alexz330@users.noreply.github.com>
2021-01-28 07:33:37 -0600

    Create ejercicio2.c

---
+++ ejercicio2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+//referenciacion: es la obtetencion  e la direccion de una variable 
+
+int main() {
+    int *ptr_intr;
+    char *ptr_char;
+    float *ptr_float;
+    return 0;
+}
+  
